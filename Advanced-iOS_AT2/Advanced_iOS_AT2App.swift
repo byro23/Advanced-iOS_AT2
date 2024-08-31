496efef8dbdf4fa0fa6ce5fe5ebe9ce7894af8a7
Byron Lester <125940003+byro23@users.noreply.github.com>
2024-08-31 12:04:18 +1000

    Initial Commit

---
+++ Advanced-iOS_AT2/Advanced_iOS_AT2App.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_iOS_AT2App.swift
+//  Advanced-iOS_AT2
+//
+//  Created by Byron Lester on 31/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_iOS_AT2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
