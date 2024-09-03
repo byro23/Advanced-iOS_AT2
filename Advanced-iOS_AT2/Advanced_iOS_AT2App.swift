38e863fcc686d6d70468d94f2d76f2cde29c550f
Byron Lester <125940003+byro23@users.noreply.github.com>
2024-09-03 16:34:44 +1000

    firestore manager and authviewmodel

--- Advanced-iOS_AT2/Advanced_iOS_AT2App.swift
+++ Advanced-iOS_AT2/Advanced_iOS_AT2App.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseCore
 
+// Firebase configuration
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
---
+++ Advanced-iOS_AT2/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  Advanced-iOS_AT2
+//
+//  Created by Byron Lester on 3/9/2024.
+//
+
+import Foundation
